e6ce1348f0c95f52bf84d1d62a33ff406ec61cee
Philippa Cowderoy <flippa@flippac.org>
2012-12-28 17:38:50 +0000

    Fix ordering on deepMetavars, add prune

--- MetaTerm.hs
+++ MetaTerm.hs
@@ -2,7 +2,7 @@
              UndecidableInstances #-}
 
 module MetaTerm (MetaTerm(..), FlatMeta(..),
-                 subst, metavars, deepMetavars, occurs) where
+                 prune, subst, metavars, deepMetavars, occurs) where
 
 import Prelude hiding (lookup)
 import Data.List (nub)
@@ -41,6 +41,16 @@ instance HasMetavars (FlatMeta t) where
   matchMeta (Meta i) = Just i
   matchMeta _ = Nothing
 
+{- Pruning -}
+
+-- Assumes t passes an occurs check!
+prune a t = case matchMeta t of
+              Nothing -> (a,t)
+              Just mv -> case Data.Map.lookup mv a of  
+                           Nothing -> (a,t)
+                           Just t' -> let (a', t'') = prune a t'
+                                       in (insert mv t'' a', t'')
+
 {- FIXME: These should generalise -}
   
 {- Substituting an assignment into a term -}
@@ -50,22 +60,23 @@ subst a (Metavar mv) = case lookup mv a of
                          Nothing -> Metavar mv
 subst a (O ty) = O $ mapChildren (subst a) ty
 
+
 {- Metavariables in a term -}
 
+-- Lists left-to-right
 metavars :: TermLevel t => MetaTerm t -> [MVIdentifier]
 metavars (Metavar mv) = [mv]
 metavars (O o) = nub $ foldChildren (++) [] (mapChildren metavars o)
 
 deepMetavars :: TermLevel t => 
                   Assignment (MetaTerm t) -> MetaTerm t -> [MVIdentifier]
-deepMetavars a t = deepMetavars' [] t
+deepMetavars a t = reverse $ deepMetavars' [] t
   where deepMetavars' visited (Metavar mv) 
           | mv `elem` visited = visited
           | otherwise = case lookup mv a of
                           Nothing -> mv:visited
                           Just t -> deepMetavars' (mv:visited) t
-        deepMetavars' v (O o) = foldChildren (\t v -> deepMetavars' v t)
-                                             v
-                                             o
+        deepMetavars' v (O o) = Prelude.foldl deepMetavars' v
+                                      $ foldChildren (:) [] o
 
 occurs mv assignment t = mv `elem` deepMetavars assignment t
